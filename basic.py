46d4da89cd80fbb812c9582ef0737cf0e373565d
andongzhengyang <30323441+andongzhengyang@users.noreply.github.com>
2020-05-20 21:47:46 -0700

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,37 @@
+import networkx as nx
+G_symmetric = nx.Graph()
+G_symmetric.add_edge('Amitabh Bachchan','Abhishek Bachchan')
+G_symmetric.add_edge('Amitabh Bachchan','Aamir Khan')
+G_symmetric.add_edge('Amitabh Bachchan','Akshay Kumar')
+G_symmetric.add_edge('Amitabh Bachchan','Dev Anand')
+G_symmetric.add_edge('Abhishek Bachchan','Aamir Khan')
+G_symmetric.add_edge('Abhishek Bachchan','Akshay Kumar')
+G_symmetric.add_edge('Abhishek Bachchan','Dev Anand')
+G_symmetric.add_edge('Dev Anand','Aamir Khan')
+
+G_asymmetric = nx.DiGraph()
+G_asymmetric.add_edge('A','B')
+G_asymmetric.add_edge('A','D')
+G_asymmetric.add_edge('C','A')
+G_asymmetric.add_edge('D','E')
+
+nx.spring_layout(G_asymmetric)
+nx.draw_networkx(G_asymmetric)
+
+G_weighted = nx.Graph()
+G_weighted.add_edge('Amitabh Bachchan','Abhishek Bachchan', weight=25)
+G_weighted.add_edge('Amitabh Bachchan','Aaamir Khan', weight=8)
+G_weighted.add_edge('Amitabh Bachchan','Akshay Kumar', weight=11)
+G_weighted.add_edge('Amitabh Bachchan','Dev Anand', weight=1)
+G_weighted.add_edge('Abhishek Bachchan','Aaamir Khan', weight=4)
+G_weighted.add_edge('Abhishek Bachchan','Akshay Kumar',weight=7)
+G_weighted.add_edge('Abhishek Bachchan','Dev Anand', weight=1)
+G_weighted.add_edge('Dev Anand','Aaamir Khan',weight=1)
+
+G = nx.MultiGraph()
+G.add_edge('A','B',relation ='neighbor')
+G.add_edge('A','B',relation='friend)
+G.add_edge('B','C', relation='neighbor')
+G.add_edge('D','C',relation='friend')
+
+MultiEdgeDataView([('A', 'B', {'relation': 'neighbor'}), ('A', 'B', {'relation': 'friend'}), ('B', 'C', {'relation': 'neighbor'}), ('B', 'D', {'relation': 'neighbor'}), ('C', 'D', {'relation': 'friend'})])
